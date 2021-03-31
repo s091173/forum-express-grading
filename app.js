b772e5144e30bb18d245129cecd766b865e4574f
Yuchi Su <astraiostor@gmail.com>
2021-03-31 22:43:36 +0800

    feat: add restaurant index page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
+app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 
