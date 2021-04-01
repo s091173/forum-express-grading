1db34d7f502cfc29dec17d8b9c6b81f0da1ca23a
Yuchi Su <astraiostor@gmail.com>
2021-04-01 21:47:57 +0800

    feat: install body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -7,6 +8,7 @@ const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
