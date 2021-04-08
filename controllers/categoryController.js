7f334da37830c29ee32f256c0de6edc2bebbe3b2
Yuchi Su <astraiostor@gmail.com>
2021-04-09 00:21:21 +0800

    feat: add admin categories delete feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -50,5 +50,15 @@ let categoryController = {
         })
     }
   },
+
+  deleteCategory: (req, res) => {
+    return Category.findByPk(req.params.id)
+      .then((category) => {
+        category.destroy()
+          .then((category) => {
+            res.redirect('/admin/categories')
+          })
+      })
+  }
 }
 module.exports = categoryController
