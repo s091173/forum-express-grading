c9a1637095837744dc2281be1b3beb2b725eea48
Yuchi Su <astraiostor@gmail.com>
2021-04-16 22:13:35 +0800

    PUT /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -15,6 +15,8 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 // 新增餐廳功能
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+// 編輯一筆餐廳資料
+router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 
 // 刪除一筆餐廳資料
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
