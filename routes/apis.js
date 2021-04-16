fe7832cdb05011e02c327097d7e6eda2a32f8c52
Yuchi Su <astraiostor@gmail.com>
2021-04-16 13:57:54 +0800

    DELETE /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -11,6 +11,9 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 // 瀏覽一筆餐廳資料
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 
+// 刪除一筆餐廳資料
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+
 // 分類總表
 router.get('/admin/categories', categoryController.getCategories)
 
